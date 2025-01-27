c85bc9a1aea08f90a2e34ca1792911e705ce0519
sowmith <sowmith.kpi@gmail.com>
2025-01-27 18:35:29 -0500

    Adding public and private subnets across availability zones

--- main.tf
+++ main.tf
@@ -14,3 +14,27 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public_subnets" {
+  count = length(data.aws_availability_zones.available.names)
+  
+  vpc_id         = aws_vpc.main.id
+  cidr_block     = element(var.public_subnet_cidr_blocks, count.index) 
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true 
+  
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count = length(data.aws_availability_zones.available.names)
+  vpc_id = aws_vpc.main.id  
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+  map_public_ip_on_launch = false
+  availability_zone = element(data.aws_availability_zones.available.names, count.index) 
+  tags = {
+    Name = "Private Subnet ${count.index}"
+  }
+}
+
